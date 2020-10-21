6e3379ef93520636fb272da014ba85676f919f42
Anushree Avasthi <Anushree_Avasthi@dell.com>
2020-10-22 00:21:08 +0530

    trial commit god save us

--- src/components/Home/ProductsTable.vue
+++ src/components/Home/ProductsTable.vue
@@ -21,9 +21,8 @@
             :search="search"
           >
             <template v-slot:item.contact="{ item }">
-              <v-icon left color="blue darken-3">mdi-microsoft-outlook</v-icon>
-              <v-icon left color="purple">mdi-microsoft-teams</v-icon>
-              <v-icon left color="blue">mdi-skype-business</v-icon>
+              <v-icon left color="blue darken-3" @click="email">mdi-microsoft-outlook</v-icon>
+              <v-icon left color="purple" @click="LoginSSH()">mdi-microsoft-teams</v-icon>
             </template>
             <template v-slot:item.info="{ item }">
               <v-icon small color="primary" @click="openDialogBox(item)">mdi-information-outline</v-icon>
@@ -73,17 +72,81 @@ export default {
           product_owner: "Anupam Piareji",
           manager: "Radhika Maramganti",
           location: "Hyderabad",
-          email: "",
-          description: "",
+          email: "Anupam_Piareji@dell.com",
+          description: "CICD automation based on different templates.",
         },
         {
-          product: "Self Service",
-          product_owner: "Anne Marie",
+          product: "Self Service Workspace Provisioning",
+          product_owner: "Anne-Marie Duffy",
           manager: "Radhika Maramganti",
           location: "Ireland",
-          email: "",
-          description: "",
+          email: "Anne_Marie@dell.com",
+          description: "Automating workspace provisioning for greenplum and providing cheaper archival solutions.",
         },
+        {
+          product: "Denodo Integration",
+          product_owner: "Atul Singh",
+          manager: "Dinesh Kopadda",
+          location: "Bangalore",
+          email: "Atul_Singh@dell.com",
+          description: "Denodo plugins and integration for faster database access.",
+        },
+         {
+          product: "BlackDuck vulnerabilities removal ",
+          product_owner: "Siva Dhandhekar",
+          manager: "Ramesh Jampula",
+          location: "Hyderabad",
+          email: "Siva_Dhandhekar@dell.com",
+          description: "Improving code quality before movement to production.",
+        },
+         {
+          product: "SAP team",
+          product_owner: "Jake Harris",
+          manager: "James Stewart",
+          location: "Round Rock",
+          email: "Jake_Harris@dell.com",
+          description: "Maintaining and managing SAP integrations across Dell.",
+        },
+         {
+          product: "Project Zero",
+          product_owner: "Ranjeetha Raja",
+          manager: "Dinesh Kopadda",
+          location: "Bangalore",
+          email: "Ranjeetha_Raja@dell.com",
+          description: "Managing and keeping checks on data extractions in Dell.",
+        },
+         {
+          product: "Jarvis",
+          product_owner: "Alfonzo Loha",
+          manager: "Patrick O'Lio",
+          location: "Malaysia",
+          email: "Alfonzo_Loha@dell.com",
+          description: "Chatbot for sales insights.",
+        },
+         {
+          product: "Incident Miner",
+          product_owner: "Marie Frankling",
+          manager: "Thomas Sujai",
+          location: "Bangalore",
+          email: "Marie_Frankling@dell.com",
+          description: "Automating incident approval and increasing the efficiency of the process.",
+        },
+         {
+          product: "Near Real Time (NRT)",
+          product_owner: "Chandra Sujana",
+          manager: "Eric Wolf",
+          location: "Mexico",
+          email: "Chandra_Sujana@dell.com",
+          description: "Increasing data access speed and updating of workspaces.",
+        },
+         {
+          product: "Sales Insights",
+          product_owner: "Vijay Kokatapatti",
+          manager: "Soma Murthy",
+          location: "Bangalore",
+          email: "Atul_singh@dell.com",
+          description: "Platform to get crucial sales insights of Dell APJC ",
+        }
       ],
     };
   },
@@ -108,4 +171,6 @@ export default {
     },
   },
 };
+
+
 </script>
